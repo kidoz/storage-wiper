25d1b4ca28a50ff79e7d4f93835fb44f7d8f7df1
Aleksandr Pavlov <ckidoz@gmail.com>
2026-08-14 12:15:47 +0300

    Poll for writability on EAGAIN in write_with_retry

--- src/util/WriteHelpers.hpp
+++ src/util/WriteHelpers.hpp
@@ -1,16 +1,27 @@
 #pragma once
 
+#include <poll.h>
 #include <unistd.h>
+
 #include <cerrno>
-#include <span>
 #include <cstdint>
+#include <span>
 
 namespace util {
 
 inline auto write_with_retry(int fd, std::span<const uint8_t> data) -> ssize_t {
     while (true) {
         const auto result = ::write(fd, data.data(), data.size());
-        if (result < 0 && (errno == EINTR || errno == EAGAIN)) {
+        if (result >= 0) {
+            return result;
+        }
+        if (errno == EINTR) {
+            continue;
+        }
+        if (errno == EAGAIN) {
+            // Non-blocking fd: wait for writability instead of busy-spinning.
+            pollfd pfd{.fd = fd, .events = POLLOUT, .revents = 0};
+            ::poll(&pfd, 1, -1);
             continue;
         }
         return result;
